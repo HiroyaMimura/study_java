2c6f3440a598a9a5d605331c412ac53dd7a30b30
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-27 08:38:20 +0000

    7章[Finish]

---
+++ Study.java
@@ -0,0 +1,32 @@
+public class Study {
+    public static void main(String[] args) {
+        int val = 8;
+        if(val <= 5) {
+            System.out.println("とても近いです");
+        } else if(val <= 10) {
+                System.out.println("近いです");
+        } else if(val <= 15) {
+                System.out.println("遠いです");
+        } else {
+                System.out.println("とても遠いです");
+        }
+        
+        String str = "red";
+        switch(str) {
+            case "red":
+                System.out.println("赤信号です");
+                break;
+            
+            case "yellow":
+                System.out.println("黄信号です");
+                break;
+                
+            case "blue":
+                System.out.println("青信号です");
+                break;
+                
+            default:
+              System.out.println("信号の色ではありません");
+        }
+    }
+}
