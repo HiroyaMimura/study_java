cebcb7136bf2f52ac1ad3fd9b335a3c811c02635
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-27 09:34:19 +0000

    8章[Finish]

--- Study.java
+++ Study.java
@@ -1,32 +1,69 @@
 public class Study {
     public static void main(String[] args) {
-        int val = 8;
-        if(val <= 5) {
-            System.out.println("とても近いです");
-        } else if(val <= 10) {
-                System.out.println("近いです");
-        } else if(val <= 15) {
-                System.out.println("遠いです");
-        } else {
-                System.out.println("とても遠いです");
+       int number1 = 100;
+       while(number1 < 50) {
+           number1 *= 2;
+           System.out.println("While =" + number1);
+       }
+       
+       int number2 = 100;
+       do {
+           number2 *= 2;
+           System.out.println("DoWhile =" + number2);
+       } while(number2 < 50);
+       
+       for(int number = 1; number <= 5; number++) {
+           System.out.println("for =" + number);
+       }
+       
+       int[] array = { 1,2,3,4,5 }; 
+       for(int number : array) {
+           System.out.println("for =" + number);
+       }
+       
+       for(int count = 0; count < 5; count++) {
+           if(count == 2) {
+               break;
+           }
+           System.out.println("break =" + count);
+       }
+       
+       for(int count = 0; count < 5; count++) {
+           if(count == 2) {
+               continue;
+           }
+           System.out.println("continue =" + count);
+       }
+       
+        //   int num = 1;
+        //   printNum(num);
+        // }
+        
+        // public static int printNum(int num) {
+        //     if(num < 50) {
+        //         num *= 2;
+        //         System.out.println("while =" + num);
+        //         printNum(num);
+        //     }
+        //     return num;
+        
+        int num = 1;
+        while(num < 5) {
+            System.out.println(num * num);
+            num++;
         }
         
-        String str = "red";
-        switch(str) {
-            case "red":
-                System.out.println("赤信号です");
-                break;
-            
-            case "yellow":
-                System.out.println("黄信号です");
-                break;
-                
-            case "blue":
-                System.out.println("青信号です");
-                break;
-                
-            default:
-              System.out.println("信号の色ではありません");
+        int[] val = {1,2,3,4};
+        for(int i = 0; i < val.length; i++) {
+            System.out.println(val[i]);
+        }
+        
+        int[] ary = {1,2,3,4};
+        for(int a : ary) {
+            if(a % 2 == 0) {
+                continue;
+            }
+            System.out.println(a);
         }
     }
 }
