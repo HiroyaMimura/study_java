85d3a3e94e49d9a20d990dab2a33f7725eab7b2a
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-01-30 04:56:42 +0000

    応用7章[Finish]

---
+++ Chapter2_8.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Chapter2_8 {
+	public static void main(String[] args) throws Exception {
+		List<List> list = new ArrayList<>();
+		list.add(new List(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new List(LocalDate.of(2021, 9, 15), "〇〇社面接", false));
+		list.add(new List(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new List(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new List(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+
+		long count = list.stream()
+				.filter(t -> !t.isDone())
+				.count();
+		System.out.println(count);
+
+		List<List> newList = list.stream()
+				.sorted()
+				.collect(Collectors.toList());
+		newList.forEach(System.out::println);
+	}
+}
