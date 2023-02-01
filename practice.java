687fd7eb4a222f36d944ec0ba9cd60ec61b9327e
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2023-02-01 04:25:23 +0000

    不具合につながる例

--- practice.java
+++ practice.java
@@ -1,14 +1,3 @@
-public class Account {
-    String accountNo;
-    
-    public boolean equals(Object o) {
-        if(o == this) return true;
-        if(o == null) return false;
-        if(!(o instanceof Account)) return false;
-        Account r = (Account)o;
-        if(!this.accountNo.trim().equals(r.accountNo.trim())) {
-            return false;
-        }
-        return true
-    }
+public class Hero {
+   public String name;
 }
