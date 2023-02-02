315147d8551df1abb79d9cc2f410a44595e2d130
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-02 17:03:11 +0900

    匿名クラス

--- study/Main.java
+++ study/Main.java
@@ -2,7 +2,12 @@ package study;
 
 public class Main {
 	public static void main(String[] args) {
-		Outer o = new Outer();
-		o.outerMethod();
+		Pocket<Object> pocket = new Pocket<>();
+		System.out.println("使い捨てのインスタンスを作りpocketに入れます");
+		pocket.put(new Object() {
+			String innerField;
+			void innerMethod() {
+			}
+		});
 	}
 }
