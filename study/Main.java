5e9210ee132113dd5036e724b951cd8d06524ae6
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-02 14:54:57 +0900

    型がなかったら

--- study/Main.java
+++ study/Main.java
@@ -1,18 +1,16 @@
 package study;
 
 public class Main {
+	public static void prints(Object a, Object b) {
+		for(int i = 0; i < (Integer)b; i++) {
+			System.out.println(a);
+		}
+	}
+
 	public static void main(String[] args) {
-		Hero h1 = new Hero("ミナト");
-		Sword s = new Sword("はがねの剣");
-
-		h1.setSword(s);
-		System.out.println("装備：" + h1.getSword().getName());
-		System.out.println("clone()で複製します");
-		Hero h2 = h1.clone();
-
-		System.out.println("コピー元の勇者の剣の名前を変えます");
-		h1.getSword().setName("ひのきの棒");
-		System.out.println("コピー元とコピー元の勇者の装備を表示します");
-		System.out.println("コピー元：" + h1.getSword().getName() + "/コピー先：" + h2.getSword().getName());
+		Object s = "こんにちは";
+		s = new Hero();
+		Object n = 1;
+		prints(s, n);
 	}
 }
