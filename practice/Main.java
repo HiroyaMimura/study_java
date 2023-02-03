ca72bdd305e9833677368a4d26842299fa7b2d6e
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-03 16:39:49 +0900

    練習問題

--- practice/Main.java
+++ practice/Main.java
@@ -1,19 +1,20 @@
 package practice;
+import java.util.List;
+import java.util.function.IntPredicate;
 
 public class Main {
 	public static void main(String[] args) {
-		StrongBox<String> sb = new StrongBox<>(KeyType.PADLOCK);
-		sb.put("鍵が開きました！");
-		String rock = "";
+		List<String> names = List.of("湊雄輔", "朝香あゆみ", "菅原拓真", "大江岳人");
+		names.stream()
+		.filter(n -> n.length() <= 4)
+		.map(n -> n + "さん")
+		.forEach(System.out::println);
 
-		for(int i = 0; i < 1024; i++) {
-			rock = sb.get();
-		}
-
-		if(rock != null) {
-			System.out.println(rock);
-		} else {
-			System.out.println("鍵は開いてません");
-		}
+		IntPredicate f1 = x -> x % 2 == 1;
+		Func2 f2 = (point, name) -> {
+			return name + "さんは" + ( point > 65 ? "合格" : "不合格");
+		};
+		System.out.println(f1.test(15));
+		System.out.println(f2.call(66, "Smith"));
 	}
 }
