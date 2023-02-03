cdc7e58dcff8efa30046135a7e13771ba930d81e
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-03 14:23:29 +0900

    ラムダ式

--- study/Main.java
+++ study/Main.java
@@ -1,26 +1,11 @@
 package study;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.function.Function;
 
 public class Main {
-	public static void partyInfo(Set<Hero> party, Hero leader, String pName) {
-		System.out.println(pName + "パーティーのリーダーは" + leader.getName());
-		System.out.print("パーティーメンバーは");
-		for(Hero h : party) {
-			System.out.println(h.getName() + " ");
-		}
-		System.out.println("");
-	}
-
 	public static void main(String[] args) {
-		Hero h1 = new Hero("ミナト");
-		Hero h2 = new Hero("アサカ");
-		Hero h3 = new Hero("スガワラ");
-		Set<Hero> party = new HashSet<>();
-		party.add(h1);
-		party.add(h2);
-		party.add(h3);
-		PartyInfoConsumer func = Main::partyInfo;
-		func.process(party, h1, "スッキリ");
+		Function<String, Integer> func =
+				(String s) -> { return s.length(); };
+		int n = func.apply("Java");
+		System.out.println("文字列[Java]は" + n + "文字です");
 	}
 }
