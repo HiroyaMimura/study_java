2ed9402bd8b226cefb24c90e3a709a820b43ae77
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-03 13:35:13 +0900

    メソッドを変数に代入して呼び出す

--- study/Main.java
+++ study/Main.java
@@ -1,7 +1,13 @@
 package study;
+import java.util.function.Function;
 
 public class Main {
-	public static int twice(int x) { return x * 2; }
-
-	public static int sub(int a, int b) { return a - b; }
+	public static Integer len(String s) {
+		return s.length();
+	}
+	public static void main(String[] args) {
+		Function<String, Integer> func = Main::len;
+		int a = func.apply("Java");
+		System.out.println("文字列[Java]は" + "文字です");
+	}
 }
