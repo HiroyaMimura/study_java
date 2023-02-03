1b510330dfbfe2223242ab23e5de839a725f69bd
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-03 13:02:23 +0900

    メソッドは関数の一種

--- study/Main.java
+++ study/Main.java
@@ -1,20 +1,7 @@
 package study;
 
-import java.util.Optional;
-
 public class Main {
-	public static Optional<String> decorate(String s, char c) {
-		String r;
-		if(s == null || s.length() == 0) {
-			r = null;
-		} else {
-			r = c + s + c;
-		}
-		return Optional.ofNullable(r);
-	}
+	public static int twice(int x) { return x * 2; }
 
-	public static void main(String[] args) {
-		Optional<String> s = decorate("", '*');
-		System.out.println(s.orElse("nullのため処理できません"));
-	}
+	public static int sub(int a, int b) { return a - b; }
 }
