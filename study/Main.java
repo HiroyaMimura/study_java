eeda83ff9aaa009758497f3ab6d4e2a3162abebd
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-03 15:35:23 +0900

    高階関数

--- study/Main.java
+++ study/Main.java
@@ -1,30 +1,23 @@
 package study;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		List<Account> list = new ArrayList<>();
+		List<Hero> heroes = new ArrayList<>();
+		Hero h1 = new Hero("ミナト");
+		h1.hp = 0;
+		Hero h2 = new Hero("アサカ");
+		h2.hp = 100;
+		heroes.add(h1);
+		heroes.add(h2);
 
-		Account a1 = new Account("1732750", AccountType.FUTSU);
-		Account a2 = new Account("1732051", AccountType.FUTSU);
-		a1.zandaka = 500000;
-		a2.zandaka = 100000;
-		list.add(a1);
-		list.add(a2);
-		System.out.println("口座番号" + list.get(0).accountNo + "の残高は" + list.get(0).zandaka + "です");
-		System.out.println("口座番号" + list.get(1).accountNo + "の残高は" + list.get(1).zandaka + "です");
-		System.out.println("listの並びは");
-		for(Account a : list) {
-			System.out.println(a.accountNo);
-		}
+		boolean anyoneKnockedOut = heroes.stream().anyMatch(h -> h.hp == 0);
 
-		Collections.sort(list, (x,y) -> (x.zandaka - y.zandaka));
-
-		System.out.println("ソート後のlistの並びは");
-		for(Account a : list) {
-			System.out.println(a.accountNo);
+		if(anyoneKnockedOut == true) {
+			System.out.println("パーティに戦闘不能の勇者がいます");
+		} else {
+			System.out.println("パーティ全員戦闘可能です");
 		}
 	}
 }
