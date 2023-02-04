507537e1f96a05ba60596ffa87be17e8ad9167fc
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-04 14:04:54 +0900

    システムプロパティの設定

--- study/Main.java
+++ study/Main.java
@@ -1,20 +1,10 @@
 package study;
 
-import java.util.Iterator;
-import java.util.Properties;
-
 public class Main {
 	public static void main(String[] args) throws Exception {
-		System.out.println("利用中のjavaバージョン");
-		System.out.println(System.getProperty("java.version"));
-
-		Properties p = System.getProperties();
-		Iterator<String> i = p.stringPropertyNames().iterator();
-		System.out.println("システムプロパティ一覧");
-		while(i.hasNext()) {
-			String key = i.next();
-			System.out.print(key + " = ");
-			System.out.println(System.getProperty(key));
-		}
+		String ver = System.getProperty("rpg.version");
+		String author = System.getProperty("rpg.author");
+		System.out.println("RPG: スッキリ魔王征伐 ver" + ver);
+		System.out.println("Developed by" + author);
 	}
 }
