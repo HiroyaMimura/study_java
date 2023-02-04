58f0b7a742078b0a8de9d63d4a857218f1382fe8
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-04 16:21:24 +0900

    練習問題

---
+++ practice/MemoryEater.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Locale;
+
+public class MemoryEater {
+	public static void main(String[] args) {
+		if(Locale.getDefault().getLanguage().equals("ja")) {
+			System.out.println("メモリーを消費しています");
+		} else {
+			System.out.println("eating memory...");
+		}
+		long[] larray = new long[1280000];
+		for(int i = 0; i < larray.length; i++) {
+			larray[i] = i;
+		}
+	}
+}
