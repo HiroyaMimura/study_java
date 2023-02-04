f37a72e4d71641b7461f3762c3617bfe1c1001a7
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-04 18:30:40 +0900

    Log4j2　ログ出力ライブラリ

--- study/Bank.java
+++ study/Bank.java
@@ -1,12 +1,13 @@
 package study;
 
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class Bank {
-	String name;
-	String address;
-
-	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
+	public static void main(String[] args) {
+		Logger logger = LogManager.getLogger(Main.class);
+		if(args.length != 2) {
+			logger.error("起動引数の数が異常：" + args.length);
+		}
 	}
 }
