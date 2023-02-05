ce239bc3cfc4db860caaa0a603f65200222edc6f
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-05 12:14:20 +0900

    ファイルの書き込み

--- study/Main.java
+++ study/Main.java
@@ -1,21 +1,13 @@
 package study;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Class<?> clazz = RefSample.class;
-		Constructor<?> cons = clazz.getConstructor(int.class);
-		RefSample rs = (RefSample)cons.newInstance(256);
-		Field f = clazz.getField("times");
-		f.set(rs, 2);
-		System.out.println(f.get(rs));
-		Method m = clazz.getMethod("hello", String.class, int.class);
-		m.invoke(rs, "reflention!", 128);
-		boolean pubc = Modifier.isPublic(clazz.getModifiers());
-		boolean finm = Modifier.isFinal(m.getModifiers());
-	}
+	public static void main(String[] args) throws IOException {
+		FileWriter fw = new FileWriter("c:¥¥rpasave.dat", true);
+		fw.write('A');
+		fw.flush();
+		fw.close();
+		}
 }
