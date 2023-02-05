4d7ce2552c4cbf19e534f9b256521208af59bf2c
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-05 15:25:14 +0900

    プロパティファイルの書き込み

--- study/Main.java
+++ study/Main.java
@@ -1,18 +1,16 @@
 package study;
 
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		FileReader fr = new FileReader("c:¥¥rgpdata.prorerties");
+		FileWriter fw = new FileWriter("c:¥¥rpgsave.properties");
 		Properties p = new Properties();
-		p.load(fr);
-		String name = p.getProperty("heroName");
-		String strHp = p.getProperty("heroHp");
-		int hp = Integer.parseInt(strHp);
-		System.out.println("勇者の名前：" + name);
-		System.out.println("勇者のHP：" + hp);
-		fr.close();
+		p.setProperty("heroName", "アサカ");
+		p.setProperty("heroHp", "62");
+		p.setProperty("heroMp", "45");
+		p.store(fw, "勇者の情報");
+		fw.close();
 	}
 }
