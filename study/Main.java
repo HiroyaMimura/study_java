7091978e4b594dc5348f22fd578ac7069bf8a527
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-05 16:24:38 +0900

    XMLファイルの読み取り

--- study/Main.java
+++ study/Main.java
@@ -1,16 +1,34 @@
 package study;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Locale loc = Locale.getDefault();
-		System.out.println(loc.getCountry() + "-" + loc.getLanguage());
-		String now = (new SimpleDateFormat()).format(new Date());
-		ResourceBundle rb = ResourceBundle.getBundle("messages");
-		System.out.println(rb.getString("CURRENT_TIME_IS") + now);
+		InputStream is = new FileInputStream("rpgsave.xml");
+		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
+			Element hero = doc.getDocumentElement();
+			Element weapon = findChildByTag(hero, "weapon");
+			Element power = findChildByTag(weapon, "power");
+			String value = power.getTextContent();
+	}
+	static Element findChildByTag(Element self, String name)
+		throws Exception {
+		NodeList children = self.getChildNodes();
+		for(int i = 0; i < children.getLength(); i++) {
+			if(children.item(i) instanceof Element) {
+			Element e = (Element)children.item(i);
+			if(e.getTagName().equals(name)) {
+				return e;
+			}
+		}
+	}
+	return null;
 	}
 }
