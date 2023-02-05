6636f0aaa8ee2201e1116e9da28f7bc59cd438fe
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-05 12:31:00 +0900

    FileOutputStreamクラス

--- study/Main.java
+++ study/Main.java
@@ -1,19 +1,14 @@
 package study;
 
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		FileReader fw = new FileReader("c:¥¥rpasave.dat");
-		System.out.println("全てのデータを読んで表示します");
-		int i = fw.read();
-		while(i != -1) {
-			char c = (char)i;
-			System.out.print(c);
-			i = fw.read();
-		}
-		System.out.println("ファイルの末尾に到達しました");
-		fw.close();
+		FileOutputStream fos =
+				new FileOutputStream("rpgsave.dat", true);
+		fos.write(65);
+		fos.flush();
+		fos.close();
 		}
 }
