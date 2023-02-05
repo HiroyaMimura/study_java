7a052b811e0a0517f9e1f6827704a7607f05a715
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-05 12:22:39 +0900

    ファイルの読み取り

--- study/Main.java
+++ study/Main.java
@@ -1,13 +1,19 @@
 package study;
 
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		FileWriter fw = new FileWriter("c:¥¥rpasave.dat", true);
-		fw.write('A');
-		fw.flush();
+		FileReader fw = new FileReader("c:¥¥rpasave.dat");
+		System.out.println("全てのデータを読んで表示します");
+		int i = fw.read();
+		while(i != -1) {
+			char c = (char)i;
+			System.out.print(c);
+			i = fw.read();
+		}
+		System.out.println("ファイルの末尾に到達しました");
 		fw.close();
 		}
 }
