5128dfe363ff2a76dba95aedbd5a69a07ae17712
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-05 12:48:12 +0900

    try-with-resources文

--- study/Main.java
+++ study/Main.java
@@ -5,19 +5,13 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter("rpgsave.dat", true);
+		try (
+			FileWriter fw = new FileWriter("rpgsave.dat", true);
+		) {
 			fw.write('A');
 			fw.flush();
 		} catch(IOException e) {
 			System.out.println("ファイル書き込みエラーです");
-		} finally {
-			if(fw != null) {
-				try {
-					fw.close();
-				} catch(IOException e2) {}
-			}
 		}
 		}
 }
