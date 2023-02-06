a45ae86f2cd5df4ed41ca6f8b037f721afa0c3f6
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-06 13:55:33 +0900

    音声形式

--- study/Main.java
+++ study/Main.java
@@ -1,16 +1,37 @@
 package study;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequencer;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		try(ZipFile file = new ZipFile("rpg.jar")) {
-			for(ZipEntry e : Collections.list(file.entries())) {
-				System.out.println(e.getName() + "size=" + e.getCompressedSize());
-			}
+	public static void main(String[] args) throws Exception {
+		Sequencer seq =  MidiSystem.getSequencer();
+		seq.open();
+		seq.setSequence(MidiSystem.getSequence(new File("xmas-bgm.mid")));
+		seq.setLoopCount(-1);
+		seq.start();
+
+		AudioInputStream ais = AudioSystem.getAudioInputStream(new File("xmas-bell.wav"));
+		Clip clip = AudioSystem.getClip();
+		clip.open();
+
+		System.out.println("メリークリスマス！");
+		System.out.println("何か入力すると3回だけベルが鳴るよ");
+
+		for(int i = 0; i < 4; i++) {
+			new Scanner(System.in).nextLine();
+			clip.start();
+			clip.setFramePosition(0);
 		}
+		clip.stop();
+		ais.close();
+		seq.stop();
+		seq.close();
 	}
 }
