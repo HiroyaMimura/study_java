01ca5b4aae5888f3f38a10935a6b5078c3c7d6f8
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-06 17:30:39 +0900

    練習問題

--- practice/Main.java
+++ practice/Main.java
@@ -1,22 +1,14 @@
 package practice;
 
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
+import java.net.Socket;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		URL url = new URL("https://dokojava.jp/favicon.ico");
-		InputStream is = url.openStream();
-		OutputStream os = new FileOutputStream("dj.ico");
-		int i = is.read();
-		while (i != -1) {
-			os.write((byte)i);
-			i = is.read();
-		}
-		is.close();
+		Socket sock = new Socket("smtp.example.com", 60025);
+		OutputStream os = sock.getOutputStream();
+		os.write("HELLO example.com¥r¥n".getBytes());
 		os.flush();
-		os.close();
+		sock.close();
 	}
 }
