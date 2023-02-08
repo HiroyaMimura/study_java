ab6b0c0c3829b7cfb0149fd9e9806e68b78bbaa1
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-08 17:11:04 +0900

    JDBCを操作する基本パターン

--- practice/Main.java
+++ practice/Main.java
@@ -1,37 +1,29 @@
 package practice;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpClient.Redirect;
-import java.net.http.HttpClient.Version;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		String url = "https://api.github.com/users/miyabilink";
-		HttpClient client = HttpClient.newBuilder()
-				.version(Version.HTTP_1_1)
-				.followRedirects(Redirect.NORMAL)
-				.build();
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(url))
-				.header("Accept", "application/vnd.github.v3+json")
-				.GET()
-				.build();
-		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		String body = response.body();
-		int status = response.statusCode();
-		if(status == 200) {
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode root = mapper.readTree(body);
-			String blog = root.get("blog").textValue();
-			System.out.println(blog);
-		} else {
-			System.out.println("ERROR: " + status);
+		try {
+			Class.forName("org.h2.Driver");
+		} catch(ClassNotFoundException e){
+			throw new IllegalStateException("ドライバのロードに失敗しました");
+		}
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:h2:~/rpgdb");
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(con != null) {
+				try {
+					con.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
