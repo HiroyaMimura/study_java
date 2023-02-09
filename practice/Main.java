db5d1081af0da95c0f62bc6f27a74a9ea409ecda
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-09 17:23:15 +0900

    更新系SQL文送信の流れ

--- practice/Main.java
+++ practice/Main.java
@@ -2,10 +2,11 @@ package practice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		try {
 			Class.forName("org.h2.Driver");
 		} catch(ClassNotFoundException e){
@@ -13,7 +14,17 @@ public class Main {
 		}
 		Connection con = null;
 		try {
-			con = DriverManager.getConnection("jdbc:h2:~/rpgdb");
+			con = DriverManager.getConnection("jdbc:h2:~/mydb");
+			PreparedStatement pstmt = con.prepareStatement("DELETE FROM MONSTERS WHERE HP <= OR NAME = ?");
+			pstmt.setInt(1,10);
+			pstmt.setString(2, "ゾンビ");
+			int r = pstmt.executeUpdate();
+			if(r != 0) {
+				System.out.println(r + "件のモンスターを削除しました");
+			} else {
+				System.out.println("該当するモンスターはありませんでした");
+			}
+			pstmt.close();
 		} catch(SQLException e) {
 			e.printStackTrace();
 		} finally {
