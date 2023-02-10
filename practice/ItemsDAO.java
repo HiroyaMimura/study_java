73039bcbe2bb7de605be09c7e62a95b53fddab7f
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-10 19:09:47 +0900

    練習問題

---
+++ practice/ItemsDAO.java
@@ -0,0 +1,47 @@
+package practice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemsDAO {
+	public static ArrayList<Item> findByMinimumPrice(int i) {
+		try {
+			Class.forName("org.h2.Driver");
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:h2:rpgdb");
+			PreparedStatement pstmt = con.prepareStatement("SELECT * FROM ITEMS WHERE PRICE >= ?");
+			pstmt.setInt(1, i);
+			ResultSet rs = pstmt.executeQuery();
+			ArrayList<Item> items = new ArrayList<>();
+			while(rs.next()) {
+				Item item = new Item();
+				item.setName(rs.getString("NAME"));
+				item.setPrice(rs.getInt("PRICE"));
+				item.setWeight(rs.getInt("WEIGHT"));
+				items.add(item);
+			}
+			rs.close();
+			pstmt.close();
+			return items;
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if(con != null) {
+				try {
+					con.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
--- practice/Main.java
+++ practice/Main.java
@@ -1,36 +1,13 @@
 package practice;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
-		try {
-			Class.forName("org.h2.Driver");
-		} catch(ClassNotFoundException e){
-			throw new IllegalStateException("ドライバのロードに失敗しました");
-		}
-		Connection con = null;
-		try {
-			con = DriverManager.getConnection("jdbc:h2:~/mydb");
-			con.setAutoCommit(false);
-
-			con.commit();
-		} catch(SQLException e) {
-			try {
-				con.rollback();
-			} catch(SQLException e2) {
-				e2.printStackTrace();
-			}
-		} finally {
-			if(con != null) {
-				try {
-					con.close();
-				} catch(SQLException e3) {
-					e3.printStackTrace();
-				}
-			}
+		System.out.println("1円以上のアイテム一覧を表示します");
+		ArrayList<Item> items = ItemsDAO.findByMinimumPrice(1);
+		for(Item item : items) {
+			System.out.printf("%10s%4d%4d", item.getName(), item.getPrice(), item.getWeight());
 		}
 	}
 }
