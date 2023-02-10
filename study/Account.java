c8e0d12f5908358434ddfe25a3ab65772f2b5efc
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-10 20:17:57 +0900

    アノテーション

--- study/Account.java
+++ study/Account.java
@@ -1,34 +1,7 @@
 package study;
 
-/**
- * 口座クラス
- * @author 湊
- * @see NewAccount
- */
 public class Account {
-	 String accountNo;
-	 /** 残高 */
-	 int zandaka;
-	 AccountType accountType;
-
-	public Account(String aNo, AccountType aType) {
-		this.accountNo = aNo;
-		this.accountType = aType;
-	}
-
-	public String getAccountNo() {
-		return accountNo;
-	}
-
-	public int getZandaka() {
-		return zandaka;
-	}
-
-	public void setZandaka(int zandaka) {
-		this.zandaka = zandaka;
-	}
-
-	public AccountType getAccountType() {
-		return accountType;
+	public int transfer(Bank bank, Account dest, int ammount) {
+		return 0;
 	}
 }
