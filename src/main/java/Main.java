b2b394c285598ba3163b1e6ec258a214c8cb48cf
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-11 10:31:02 +0900

    Maven

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+package src.main.java;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("スッキリ魔王征伐 ver1.0.0 by minato");
+		System.out.println("Mavenで鋭意開発中！待て次号！");
+	 }
+}
