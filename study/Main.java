c88b0c3faa4ac7c5fc1994bf08e71ba98d53a33b
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-15 14:58:46 +0900

    Singletonパターン

--- study/Main.java
+++ study/Main.java
@@ -1,16 +1,8 @@
 package study;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
 	public static void main(String[] args) throws Exception {
-		System.out.println("起動完了");
-		ServerSocket svSock = new ServerSocket(39684);
-		Socket sock = svSock.accept();
-		System.out.println(sock.getInetAddress() + "からの接続");
-		sock.getOutputStream().write("WELCOME".getBytes());
-		sock.getOutputStream().flush();
-		sock.close();
+		OnlyoneFlower f1 = new OnlyoneFlower();
+		OnlyoneFlower f2 = new OnlyoneFlower();
 	}
 }
---
+++ study/SingletonFlower.java
@@ -0,0 +1,12 @@
+package study;
+
+public class SingletonFlower {
+	private static SingletonFlower theInstance;
+	private SingletonFlower() {}
+	public static SingletonFlower getInstance() {
+		if(theInstance == null) {
+			theInstance = new SingletonFlower();
+		}
+		return theInstance;
+	}
+}
