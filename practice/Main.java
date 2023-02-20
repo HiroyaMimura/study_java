5963800c85fc1ed53e2f126f8431ef84786d8dbc
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-20 14:51:43 +0900

    練習問題

--- practice/Main.java
+++ practice/Main.java
@@ -1,13 +1,10 @@
 package practice;
 
-import java.util.ArrayList;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("1円以上のアイテム一覧を表示します");
-		ArrayList<Item> items = ItemsDAO.findByMinimumPrice(1);
-		for(Item item : items) {
-			System.out.printf("%10s%4d%4d", item.getName(), item.getPrice(), item.getWeight());
-		}
+		Mylogger logger1 = ();
+		logger1.log("first");
+		Mylogger logger2 = ();
+		logger2.log("second");
 	}
 }
