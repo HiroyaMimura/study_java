c21fef6e2018b12ed4b8e4910b972ed263fe4632
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-21 16:22:07 +0900

    AtomicBoolean

--- study/PrintingThread.java
+++ study/PrintingThread.java
@@ -1,10 +1,16 @@
 package study;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PrintingThread extends Thread {
+	final AtomicBoolean stopReq = new AtomicBoolean(false);
+
 	public void run() {
 		for(int i = 9; i >= 0; i--) {
+			if(this.stopReq.get()) {
+				break;
+			}
 			System.out.println(i + "..");
 			try {
 				TimeUnit.SECONDS.sleep(1);
