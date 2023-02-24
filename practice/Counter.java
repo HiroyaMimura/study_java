7edb6d352f790f93867e6066ae28659f7fc5f4c5
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-24 17:33:37 +0900

    練習問題

---
+++ practice/Counter.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Counter {
+	private long count = 0;
+	public void add(long i) {
+		System.out.println("足し算をします");
+		synchronized(this) {
+			this.count += i;
+		}
+	}
+	public void mul(long i) {
+		System.out.println("掛け算をします");
+		synchronized(this) {
+			this.count *= i;
+		}
+	}
+}
