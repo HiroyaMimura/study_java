9cd9f77645ea39186eed92521334b86e24ecafda
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-24 17:23:56 +0900

    練習問題

---
+++ practice/CountUpThread.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class CountUpThread extends Thread {
+	public void run() {
+		for(int i = 0; i <= 50; i++) {
+			System.out.println(i);
+		}
+	}
+}
--- practice/Main.java
+++ practice/Main.java
@@ -1,10 +1,9 @@
 package practice;
 
 public class Main {
-	public static void main(String[] args) {
-		Mylogger logger1 = ();
-		logger1.log("first");
-		Mylogger logger2 = ();
-		logger2.log("second");
+	public static void main(String[] args) throws Exception {
+		new CountUpThread().start();
+		new CountUpThread().start();
+		new CountUpThread().start();
 	}
 }
