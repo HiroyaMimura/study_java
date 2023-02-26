11be48ae15877d8c32b368febc2fb3c2f2e0c544
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-26 15:39:25 +0900

    フレームの表示

--- practice/Main.java
+++ practice/Main.java
@@ -1,9 +1,13 @@
 package practice;
 
+import javax.swing.JFrame;
+
 public class Main {
-	public static void main(String[] args) throws Exception {
-		new CountUpThread().start();
-		new CountUpThread().start();
-		new CountUpThread().start();
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("初めてのSwing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 200);
+		frame.setVisible(true);
+		System.out.println("フレームを表示");
 	}
 }
