1f7f6c428fd9785d6c4c759d5f0f73c1263662fb
HiroyaMimura <v36.hm1121@gmail.com>
2023-02-26 15:48:38 +0900

    ウィジェットの生成と追加

--- practice/Main.java
+++ practice/Main.java
@@ -1,12 +1,23 @@
 package practice;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("初めてのSwing");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 200);
+
+		frame.setLayout(new FlowLayout());
+		JLabel label = new JLabel("Hello World!!");
+		frame.add(label);
+		JButton button = new JButton("押してね");
+		frame.add(button);
+
 		frame.setVisible(true);
 		System.out.println("フレームを表示");
 	}
